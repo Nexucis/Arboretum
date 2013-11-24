a23cd8d676a8f7c0b10b2468636fef7b81e9d0d5
RodolpheFreby <rfreby@gmail.com>
2013-11-24 13:48:26 +0000

    Ajout de la deuxieme activite pour la visite de l'arboretum, bug au moment du switch d'une activite a une autre. Ajout des boutons pour le menu principal Rodolphe

--- src/com/ricm/arboretum/MainActivity.java
+++ src/com/ricm/arboretum/MainActivity.java
@@ -2,17 +2,28 @@ package com.ricm.arboretum;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener {
+	
+	private Button btnArboretum;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Toast.makeText(this, "Oh, un toast",Toast.LENGTH_LONG).show();
 		Toast.makeText(this, "Oh, un autre toast",Toast.LENGTH_LONG).show();
+		
+		btnArboretum = (Button) findViewById(R.id.btnArboretum);
+		btnArboretum.setOnClickListener(this);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,5 +31,45 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	@Override
+	  protected void onRestart() {
+	    super.onRestart();
+	  }
+
+	  @Override
+	  protected void onStart() {
+	    super.onStart();
+	  }
+
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	  }
+
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	  }
+
+	  @Override
+	  protected void onStop() {
+	    super.onStop();
+	  }
+
+	  @Override
+	  protected void onDestroy() {
+	    super.onDestroy();
+	  }
+
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, Visite.class);
+		startActivity(intent);
+	}
+	
+
+	
 
 }
---
+++ src/com/ricm/arboretum/Visite.java
@@ -0,0 +1,46 @@
+package com.ricm.arboretum;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Visite extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.visite);
+	     
+	   }
+
+	@Override
+	  protected void onRestart() {
+	    super.onRestart();
+	  }
+
+	  @Override
+	  protected void onStart() {
+	    super.onStart();
+	  }
+
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	  }
+
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	  }
+
+	  @Override
+	  protected void onStop() {
+	    super.onStop();
+	  }
+
+	  @Override
+	  protected void onDestroy() {
+	    super.onDestroy();
+	  }
+
+	
+}
