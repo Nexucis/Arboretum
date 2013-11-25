6794e79e7f12b83ba90cd777d4146d5cc6d970c8
RodolpheFreby <rfreby@gmail.com>
2013-11-25 09:06:30 +0000

    Ajout des deux fichiers responsables du crash de william

---
+++ src/com/ricm/arboretum/Direction.java
@@ -0,0 +1,14 @@
+package com.ricm.arboretum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Direction extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+		
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.direction);
+	     
+	   }
+
+}
