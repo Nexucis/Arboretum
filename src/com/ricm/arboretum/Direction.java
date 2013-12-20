28a229ac4e276dbac9a9825cd50e39aa1ccfb1c0
Rodolphe <rfreby@gmail.com>
2013-12-20 15:39:14 +0100

    ajout du code pour la map

--- src/com/ricm/arboretum/Direction.java
+++ src/com/ricm/arboretum/Direction.java
@@ -1,14 +1,65 @@
 package com.ricm.arboretum;
 
-import android.app.Activity;
+
+import java.io.File;
+import java.util.List;
+
+import org.mapsforge.android.maps.MapActivity;
+import org.mapsforge.android.maps.MapView;
+import org.mapsforge.android.maps.overlay.ListOverlay;
+import org.mapsforge.android.maps.overlay.Marker;
+import org.mapsforge.android.maps.overlay.OverlayItem;
+import org.mapsforge.core.model.GeoPoint;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
-public class Direction extends Activity{
-	protected void onCreate(Bundle savedInstanceState) {
+public class Direction extends MapActivity implements LocationListener{
+
+	LocationManager locationManager;
+	MapView mapView;
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
 		
-	     super.onCreate(savedInstanceState);
-	     setContentView(R.layout.direction);
-	     
-	   }
+		File map = new File(Environment.getExternalStorageDirectory().toString() + "/campus.map");
+		mapView = new MapView(this);
+		mapView.setMapFile(map);
+		setContentView(mapView);
+	}
+	
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
