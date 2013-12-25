73668fb1186b662fb7161d914ce043bdf1aa1e78
barbierj <jerome.barbier26@gmail.com>
2013-12-25 15:24:10 +0100

    rajout image logo

--- src/com/ricm/arboretum/Direction.java
+++ src/com/ricm/arboretum/Direction.java
@@ -24,107 +24,107 @@ import android.widget.Toast;
 
 public class Direction extends MapActivity implements LocationListener{
 
-	 private LocationManager locationManager;
-	 private MapView mapView;
-	 private SensorManager sm;
-	 private Sensor my_sensor;
-	 private Marker mMe;
-	 private Marker mArbo;
+	private LocationManager locationManager;
+	private MapView mapView;
+	private SensorManager sm;
+	private Sensor my_sensor;
+	private Marker mMe;
+	private Marker mArbo;
 
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		  
-		  
-		   File map = new File(Environment.getExternalStorageDirectory().toString() + "/grenoble.map");
-		  
-		   mapView = new MapView(this);
 
-		  //Toast.makeText(this,map.exists()+"", Toast.LENGTH_LONG).show();
-		  //Toast.makeText(this,"Welcome to my application", Toast.LENGTH_LONG).show();
-		  
-		  mapView.setMapFile(map);
 
-		  
-		  //on récupère la position actuelle : 0,0 permet de ne pas "fausser" l'affichage
-		  GeoPoint myPos = new GeoPoint(0,0);
-		  GeoPoint locArbo = new GeoPoint(45.193626,5.7783196);
-		  //on définie notre image sur la map qui correspond à notre emplacement
-		  mMe = new Marker(myPos,getResources().getDrawable(R.drawable.location_oriented));
-		  mArbo = new Marker (locArbo,getResources().getDrawable(R.drawable.location));
-		  ListOverlay mOverlayList = new ListOverlay(); 
-		  List<OverlayItem> overlayItems = mOverlayList.getOverlayItems();
-		  mapView.getOverlays().add(mOverlayList);
+		File map = new File(Environment.getExternalStorageDirectory().toString() + "/grenoble.map");
 
-		  overlayItems.add(mMe);
-		  overlayItems.add(mArbo);
-		  //on rend la map cliquable
-		  mapView.setClickable(true);
-		  //on rend la map zoomable
-		  mapView.setBuiltInZoomControls(true);
-		  //on centre la map sur notre position
-		  mapView.getMapViewPosition().setCenter(myPos);
-		  
-		  locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
-		  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
-		  
-		  
-		  sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		      
-		  if (sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
-		   //success! there's a magnetometer
-		  }
-		  
-		  else Toast.makeText(this,"Noob accélérometer", Toast.LENGTH_LONG).show();
+		mapView = new MapView(this);
 
-		  
-		  setContentView(mapView);
+		//Toast.makeText(this,map.exists()+"", Toast.LENGTH_LONG).show();
+		//Toast.makeText(this,"Welcome to my application", Toast.LENGTH_LONG).show();
+
+		mapView.setMapFile(map);
+
+
+		//on récupère la position actuelle : 0,0 permet de ne pas "fausser" l'affichage
+		GeoPoint myPos = new GeoPoint(0,0);
+		GeoPoint locArbo = new GeoPoint(45.193626,5.7783196);
+		//on définie notre image sur la map qui correspond à notre emplacement
+		mMe = new Marker(myPos,getResources().getDrawable(R.drawable.location_oriented));
+		mArbo = new Marker (locArbo,getResources().getDrawable(R.drawable.location));
+		ListOverlay mOverlayList = new ListOverlay(); 
+		List<OverlayItem> overlayItems = mOverlayList.getOverlayItems();
+		mapView.getOverlays().add(mOverlayList);
+
+		overlayItems.add(mMe);
+		overlayItems.add(mArbo);
+		//on rend la map cliquable
+		mapView.setClickable(true);
+		//on rend la map zoomable
+		mapView.setBuiltInZoomControls(true);
+		//on centre la map sur notre position
+		mapView.getMapViewPosition().setCenter(myPos);
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
+
+
+		sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+		if (sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+			//success! there's a magnetometer
+		}
+
+		else Toast.makeText(this,"Noob accélérometer", Toast.LENGTH_LONG).show();
+
+
+		setContentView(mapView);
 	}
-	 @Override
-	 protected void onResume() {
-	  super.onResume();
-	  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this); 
-	  locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this); 
-	 }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this); 
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this); 
+	}
 
-	 @Override
-	 protected void onPause() {
-	  super.onPause();
-	  locationManager.removeUpdates(this); 
-	 }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this); 
+	}
 
-	 @Override
-	 public void onLocationChanged(Location arg0) {
-	  // TODO Auto-generated method stub
-	  Log.v("Location", locationManager.toString());
-	  
-	  //je récupère ma position actuel
-	  GeoPoint my_pos=new GeoPoint(arg0.getLatitude(),arg0.getLongitude());
-	  mMe.setGeoPoint(my_pos);
-	  //je l'ajoute à ma map
-	  mapView.getMapViewPosition().setCenter(my_pos);
-	  //refresh
-	  mapView.redraw();
-	  
-	 }
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		Log.v("Location", locationManager.toString());
 
-	 @Override
-	 public void onProviderDisabled(String arg0) {
-	  // TODO Auto-generated method stub
-	  //Log.v("Location", locationManager.toString()); 
-	      
-	 }
+		//je récupère ma position actuel
+		GeoPoint my_pos=new GeoPoint(arg0.getLatitude(),arg0.getLongitude());
+		mMe.setGeoPoint(my_pos);
+		//je l'ajoute à ma map
+		mapView.getMapViewPosition().setCenter(my_pos);
+		//refresh
+		mapView.redraw();
 
-	 @Override
-	 public void onProviderEnabled(String arg0) {
-	  // TODO Auto-generated method stub
-	  
-	 }
+	}
 
-	 @Override
-	 public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-	  // TODO Auto-generated method stub
-	  
-	 }
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		//Log.v("Location", locationManager.toString()); 
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
+	}
 
 
 }
