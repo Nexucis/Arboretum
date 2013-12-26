dc259ebe3601c90c8b202c9dab16f9d8b7b704a8
Rodolphe <Rodolphe@192.168.1.14>
2013-12-26 20:09:42 +0100

    Ontap OP

--- src/com/ricm/arboretum/Direction2.java
+++ src/com/ricm/arboretum/Direction2.java
@@ -23,6 +23,10 @@ import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+
+
 public class Direction2 extends MapActivity implements LocationListener{
 
 	private MapView mapView;
@@ -31,6 +35,42 @@ public class Direction2 extends MapActivity implements LocationListener{
 	private Sensor my_sensor;
 	private OverlayItem item;
 	private OverlayItem item2;
+	
+	
+	 private static class MyItemizedOverlay extends ArrayItemizedOverlay {
+         private final Context context;
+
+         /**
+          * Constructs a new MyItemizedOverlay.
+          * 
+          * @param defaultMarker
+          *            the default marker (may be null).
+          * @param context
+          *            the reference to the application context.
+          */
+         MyItemizedOverlay(Drawable defaultMarker, Context context) {
+                 super(defaultMarker);
+                 this.context = context;
+         }
+
+         /**
+          * Handles a tap event on the given item.
+          */
+         @Override
+         protected boolean onTap(int index) {
+                 OverlayItem item = createItem(index);
+                 if (item != null) {
+                         Builder builder = new AlertDialog.Builder(this.context);
+                         builder.setIcon(android.R.drawable.ic_menu_info_details);
+                         builder.setTitle(item.getTitle());
+                         builder.setMessage(item.getSnippet());
+                         builder.setPositiveButton("OK", null);
+                         builder.show();
+                 }
+                 return true;
+         }
+ }
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +96,7 @@ public class Direction2 extends MapActivity implements LocationListener{
 		Drawable defaultMarker = getResources().getDrawable(R.drawable.location); 
 		Drawable location = getResources().getDrawable(R.drawable.location_oriented); 
 
-		ArrayItemizedOverlay itemizedOverlay = new ArrayItemizedOverlay(defaultMarker);
+		ArrayItemizedOverlay itemizedOverlay = new MyItemizedOverlay(defaultMarker, this);
 		ArrayItemizedOverlay itemizedOverlay2 = new ArrayItemizedOverlay(location);
 
 		// create a GeoPoint with the latitude and longitude coordinates
