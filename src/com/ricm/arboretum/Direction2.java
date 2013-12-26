c7bfa1ef169ad4d5316f36f522f152ce7e40228c
Rodolphe <Rodolphe@192.168.1.14>
2013-12-26 17:26:43 +0100

    Ajout du marker
    /!\ ATTENTION : il faut TOUJOURS ajouter les markers APRES
    setContentView(mapView)
    sinon ça crash !

--- src/com/ricm/arboretum/Direction2.java
+++ src/com/ricm/arboretum/Direction2.java
@@ -4,11 +4,17 @@ import java.io.File;
 
 import org.mapsforge.android.maps.MapActivity;
 import org.mapsforge.android.maps.MapView;
+import org.mapsforge.android.maps.overlay.ArrayItemizedOverlay;
+import org.mapsforge.android.maps.overlay.ItemizedOverlay;
+import org.mapsforge.android.maps.overlay.OverlayItem;
+import org.mapsforge.android.maps.overlay.OverlayList;
+import org.mapsforge.core.GeoPoint;
 import org.mapsforge.map.reader.header.FileOpenResult;
 
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 
 public class Direction2 extends MapActivity{
 
@@ -17,6 +23,8 @@ public class Direction2 extends MapActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+
 
 		//chargement du fichier map
 		File map = new File(Environment.getExternalStorageDirectory().toString() + "/grenoble.map");
@@ -32,6 +40,23 @@ public class Direction2 extends MapActivity{
 		//affichage de la map
 		setContentView(mapView);	
 
+		
+		Drawable defaultMarker = getResources().getDrawable(R.drawable.location); 
+
+		ArrayItemizedOverlay itemizedOverlay = new ArrayItemizedOverlay(defaultMarker);
+
+		// create a GeoPoint with the latitude and longitude coordinates
+		GeoPoint geoPoint = new GeoPoint(45.193626,5.7783196);
+
+		// create an OverlayItem with title and description
+		OverlayItem item = new OverlayItem(geoPoint, "MyPoint", "This is my own point.");
+		item.setMarker(ItemizedOverlay.boundCenter(defaultMarker));
+
+		// add the OverlayItem to the ArrayItemizedOverlay
+		itemizedOverlay.addItem(item);
+
+		// add the ArrayItemizedOverlay to the MapView
+		mapView.getOverlays().add(itemizedOverlay);
 
 	}
 
