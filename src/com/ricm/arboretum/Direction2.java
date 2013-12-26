234aefa411f4e483991ce0d37fab1bc053edf5ee
Rodolphe <Rodolphe@192.168.1.14>
2013-12-26 17:54:53 +0100

    ajout du GPS

--- src/com/ricm/arboretum/Direction2.java
+++ src/com/ricm/arboretum/Direction2.java
@@ -11,19 +11,31 @@ import org.mapsforge.android.maps.overlay.OverlayList;
 import org.mapsforge.core.GeoPoint;
 import org.mapsforge.map.reader.header.FileOpenResult;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 
-public class Direction2 extends MapActivity{
+public class Direction2 extends MapActivity implements LocationListener{
 
 	private MapView mapView;
+	private LocationManager locationManager;
+	private SensorManager sm;
+	private Sensor my_sensor;
+	private OverlayItem item;
+	private OverlayItem item2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 
 
 		//chargement du fichier map
@@ -40,39 +52,85 @@ public class Direction2 extends MapActivity{
 		//affichage de la map
 		setContentView(mapView);	
 
-		
+
 		Drawable defaultMarker = getResources().getDrawable(R.drawable.location); 
+		Drawable location = getResources().getDrawable(R.drawable.location_oriented); 
 
 		ArrayItemizedOverlay itemizedOverlay = new ArrayItemizedOverlay(defaultMarker);
+		ArrayItemizedOverlay itemizedOverlay2 = new ArrayItemizedOverlay(location);
 
 		// create a GeoPoint with the latitude and longitude coordinates
-		GeoPoint geoPoint = new GeoPoint(45.193626,5.7783196);
+		GeoPoint myPos = new GeoPoint(0,0);
+		GeoPoint arbo = new GeoPoint(45.193626,5.7783196);
 
 		// create an OverlayItem with title and description
-		OverlayItem item = new OverlayItem(geoPoint, "MyPoint", "This is my own point.");
+		item = new OverlayItem(arbo, "Arboretum", "Localisation de l'arboretum.");
+		item2 = new OverlayItem(myPos, "Moi", "Ma position.");
 		item.setMarker(ItemizedOverlay.boundCenter(defaultMarker));
+		item2.setMarker(ItemizedOverlay.boundCenter(location));
 
 		// add the OverlayItem to the ArrayItemizedOverlay
 		itemizedOverlay.addItem(item);
+		itemizedOverlay2.addItem(item2);
+
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
+		sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+		if (sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+			//success! there's a magnetometer
+		}else{
+			Toast.makeText(this,"Noob accélérometer", Toast.LENGTH_LONG).show();
+		}
 
 		// add the ArrayItemizedOverlay to the MapView
 		mapView.getOverlays().add(itemizedOverlay);
+		mapView.getOverlays().add(itemizedOverlay2);
 
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this); 
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this); 
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
+		locationManager.removeUpdates(this); 
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onPause();
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		GeoPoint my_pos=new GeoPoint(arg0.getLatitude(),arg0.getLongitude());
+		item2.setPoint(my_pos);
+		mapView.refreshDrawableState();
+		// TODO Auto-generated method stub
+
+
+	}
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
 	}
 }
 
