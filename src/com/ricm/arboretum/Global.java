14b39bcd88b1935c3126c6970b0984ffa11e40a8
Rodolphe <Rodolphe@192.168.1.14>
2013-12-29 17:08:59 +0100

    modif du nom par défaut

--- src/com/ricm/arboretum/Global.java
+++ src/com/ricm/arboretum/Global.java
@@ -2,7 +2,7 @@ package com.ricm.arboretum;
 
 public class Global {
 
-	public static String nomFichier="default.html";
+	public static String nomFichier="arboretum.html";
 	
 	public Global() {
 		// TODO Auto-generated constructor stub
