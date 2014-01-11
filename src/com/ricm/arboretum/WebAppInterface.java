14e442de3004cc352fb168e4661ee10c2e113c16
Rodolphe <Rodolphe@192.168.1.14>
2014-01-11 16:06:31 +0100

    Test pour le tts

--- src/com/ricm/arboretum/WebAppInterface.java
+++ src/com/ricm/arboretum/WebAppInterface.java
@@ -15,7 +15,13 @@ public class WebAppInterface {
     @JavascriptInterface
     public void readText(String text) {
         //Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
-        DescriptionArbo.speakOut(text);
+        //DescriptionArbo.speakOut(text);
+    	DescriptionArbo.speakOut("premier texte, on fait un essai");
+    	DescriptionArbo.speakOut("Puis un second essai");
+    	DescriptionArbo.speakOut("Et enfin un troisieme");
+    	DescriptionArbo.speakOut("Il ne semble pas y avoir de probleme");
+    	DescriptionArbo.speakOut("Cependant mettre plus de texte permettrait");
+    	DescriptionArbo.speakOut("De se faire une meilleur idée");
     }
     
 }
