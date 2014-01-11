7eb0c77734f5a07500e54f5df9496b2e9c77f07f
Rodolphe <Rodolphe@192.168.1.14>
2014-01-11 23:18:57 +0100

    Nettoyage des imports d'un fichier

--- src/com/ricm/arboretum/SousMenuVisite.java
+++ src/com/ricm/arboretum/SousMenuVisite.java
@@ -1,15 +1,12 @@
 package com.ricm.arboretum;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 public class SousMenuVisite extends Nfc_Activity implements OnClickListener {
 
